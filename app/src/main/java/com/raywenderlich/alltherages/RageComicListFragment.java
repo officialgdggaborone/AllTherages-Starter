3d8d94a4929f450b00a71d45cc9543438cfe6cea
kesegotumisang <201101433@ub.ac.bw>
2019-08-11 16:17:00 +0200

    Update

--- app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
+++ app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
@@ -1,8 +1,10 @@
+
+
 /*
- * Copyright (c) 2015 Razeware LLC
+ * Copyright (c) 2019.  GDG Gaborone
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ *  of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -84,8 +86,8 @@ public class RageComicListFragment extends Fragment {
       super(itemView);
 
       // Get references to image and name.
-      mImageView = (ImageView) itemView.findViewById(R.id.comic_image);
-      mNameTextView = (TextView) itemView.findViewById(R.id.name);
+      mImageView = itemView.findViewById(R.id.comic_image);
+      mNameTextView = itemView.findViewById(R.id.name);
     }
 
     private void setData(int imageResId, String name) {
